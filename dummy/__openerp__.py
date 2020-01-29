b6c9fec6d9504f9eae4c54ee0cf3a7f8496d13f5
Ivan Yelizariev <yelizariev@it-projects.info>
2020-01-29 12:40:06 +0500

    Update __openerp__.py

--- dummy/__openerp__.py
+++ dummy/__openerp__.py
@@ -4,6 +4,7 @@
     "summary": """The module does nothing""",
     "category": "Hidden",
     "images": ['images/1.jpg', 'images/2.jpg','images/3.jpg','images/4.jpg'],
+    "live_test_url": "https://example.com/v10",
     "version": "1.0.2",
     "application": False,
 
